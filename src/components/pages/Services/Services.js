046f3f2acb06cb3d2c2e38fea990a59574d5037a
Danglhp <dangcongly06@gmail.com>
2023-12-08 15:39:14 +0700

    update manage printer page

--- src/components/pages/Services/Services.js
+++ src/components/pages/Services/Services.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import HeroSection from '../../HeroSection';
+import SforMPrinter from '../../SforMPrinter';
 import { homeObjOne, homeObjThree} from './Data';
 
 function Services() {
   return (
     <>
-      <HeroSection {...homeObjOne} />
-      <HeroSection {...homeObjThree} />
+      <SforMPrinter {...homeObjOne} />           
     </>
   );
 }
