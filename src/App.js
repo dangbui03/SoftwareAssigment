467a23e5e7a90178ac02ca284bac64fe521680ab
DarkChan <buihohaidang@gmail.com>
2023-12-11 17:09:16 +0700

    4 page printerinfo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import SignUp from './components/pages/SignUp/SignUp';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './components/pages/SignUp/Data';
+import PrinterInfo1 from './components/pages/PrinterInfo1/Info'
+import PrinterInfo2 from './components/pages/PrinterInfo2/Info'
+import PrinterInfo3 from './components/pages/PrinterInfo3/Info'
+import PrinterInfo4 from './components/pages/PrinterInfo4/Info'
+
 function App() {
   return (
     <Router>
@@ -16,6 +21,10 @@ function App() {
         <Route path='/services' element={<Services />} />
         <Route path='/products' element={<Products />} />
         <Route path='/sign-up' element={<Login />} />
+        <Route path='/printer-info1' element={<PrinterInfo1/>} />
+        <Route path='/printer-info2' element={<PrinterInfo2/>} />
+        <Route path='/printer-info3' element={<PrinterInfo3/>} />
+        <Route path='/printer-info4' element={<PrinterInfo4/>} />
       </Routes> 
     </Router>
   );
---
+++ src/components/pages/PrinterInfo3/Data.js
@@ -0,0 +1,22 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Printer Informations',
+    headline: 'Printer 3: Toshiba e-Studio 5008A',
+    description: `
+    Printer 3: 3003
+    Brand: Toshiba
+    Model: Toshiba e-Studio 5008A
+    Status: In use
+    Location: 
+    Campus name: Đại học bách khoa lý thường kiệt
+    Building name: A4 building Corner 
+    Room number: A4-404
+    Description: open from 7am - 4pm
+    `,
+    imgStart: '',
+    img: 'images/May-Photocopy-Toshiba-e-Studio-5008A.png',
+    alt: 'Smart Printer'
+  };
+  
